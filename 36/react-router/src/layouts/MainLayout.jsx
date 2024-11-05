4313b58ba4dcb3b8b940d03c42dcf813e725f3bd
Fiqi Fitransyah <fiqi@3dbyggeri.com>
2024-11-05 22:20:03 +0700

    Bab 36 - React router plus login

--- 36/react-router/src/layouts/MainLayout.jsx
+++ 36/react-router/src/layouts/MainLayout.jsx
@@ -1,11 +1,40 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 function MainLayout() {
+  let navigate = useNavigate()
+
+  const showDashboard = () => {
+    if (localStorage.getItem("access_token")) {
+      return <Link to="/dashboard">Dashboard</Link>
+    }
+  }
+
+  const login = () => {
+    localStorage.setItem("access_token", "123qweasd")
+    navigate("/dashboard")
+  }
+
+  const logout = () => {
+    localStorage.removeItem("access_token")
+    navigate("/")
+  }
+
   return (
     <div>
       <nav style={{ display: 'flex', gap: '16px'}}>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        { showDashboard() }
+        {
+          localStorage.getItem("access_token")
+          &&
+          <button onClick={() => logout()}>Logout</button>
+        }
+        {
+          !localStorage.getItem("access_token")
+          &&
+          <button onClick={() => login()}>Login</button>
+        }
       </nav>
       <Outlet />
     </div>
